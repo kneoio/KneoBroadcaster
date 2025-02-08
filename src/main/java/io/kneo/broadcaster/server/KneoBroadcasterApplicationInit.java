ae44173eea5b4f7f8da6ad0c2249d45befd0debc
kaira <justaidajam@gmail.com>
2025-02-08 16:11:31 +0000

    #

--- src/main/java/io/kneo/broadcaster/server/KneoBroadcasterApplicationInit.java
+++ src/main/java/io/kneo/broadcaster/server/KneoBroadcasterApplicationInit.java
@@ -2,13 +2,21 @@ package io.kneo.broadcaster.server;
 
 import io.kneo.broadcaster.controller.RadioController;
 import io.kneo.broadcaster.controller.SoundFragmentController;
-import io.kneo.core.server.ApplicationInit;
 
+import io.kneo.core.server.AbstractApplicationInit;
+import io.quarkus.runtime.StartupEvent;
+import io.vertx.ext.web.Router;
+import io.vertx.mutiny.pgclient.PgPool;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @ApplicationScoped
-public class KneoBroadcasterApplicationInit extends ApplicationInit {
+public class KneoBroadcasterApplicationInit extends AbstractApplicationInit {
+    private static final Logger LOGGER = LoggerFactory.getLogger(KneoBroadcasterApplicationInit.class);
 
     @Inject
     SoundFragmentController soundFragmentController;
@@ -16,11 +24,29 @@ public class KneoBroadcasterApplicationInit extends ApplicationInit {
     @Inject
     RadioController radioController;
 
-    @Override
-    protected void setupRoutes() {
-        super.setupRoutes();
+    @Inject
+    protected Router router;
+
+    @Inject
+    public KneoBroadcasterApplicationInit(PgPool client)  {
+        super(client);
+    }
+
+    //For DI
+    public KneoBroadcasterApplicationInit() {
+        super(null);
+    }
+
+    public void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...{}", EnvConst.APP_ID);
+        super.setupRoutes(router);
         soundFragmentController.setupRoutes(router);
         radioController.setupRoutes(router);
-        logRegisteredRoutes();
+        logRegisteredRoutes(router);
+
+        if (EnvConst.DEV_MODE) {
+            LOGGER.info(EnvConst.APP_ID + "'s dev mode enabled");
+            //checkDatabaseConnection();
+        }
     }
 }

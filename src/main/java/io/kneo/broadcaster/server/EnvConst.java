624af266954ce817cdbc39885452ae42b871ed1e
kaira <justaidajam@gmail.com>
2025-06-17 16:34:14 +0100

    #

--- src/main/java/io/kneo/broadcaster/server/EnvConst.java
+++ src/main/java/io/kneo/broadcaster/server/EnvConst.java
@@ -2,6 +2,6 @@ package io.kneo.broadcaster.server;
 
 public class EnvConst {
     public static final String APP_ID = "KneoBroadcaster" ;
-    public static final String VERSION = "1.1.8" ;
+    public static final String VERSION = "1.1.9" ;
     public static boolean DEV_MODE = true;
 }

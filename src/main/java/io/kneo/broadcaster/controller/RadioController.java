cebe689a22f9e6ad4b2a5cd82d4fbb6947acb750
kaira <justaidajam@gmail.com>
2025-09-07 12:19:55 +0100

    #

--- src/main/java/io/kneo/broadcaster/controller/RadioController.java
+++ src/main/java/io/kneo/broadcaster/controller/RadioController.java
@@ -48,7 +48,7 @@ public class RadioController {
         String brand = rc.pathParam("brand").toLowerCase();
         String userAgent = rc.request().getHeader("User-Agent");
         String clientIP = rc.request().getHeader("stream-connecting-ip");
-
+        LOGGER.info(">>>>>>>>>>>>>>> client IP {}", clientIP);
         geoService.recordAccessWithGeolocation(brand, userAgent, clientIP)
                 .chain(country -> service.getPlaylist(brand, userAgent, false))
                 .onItem().transform(IStreamManager::generatePlaylist)

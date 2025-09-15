8978fef39eac6ea862155a7c2d295d565081a038
kaira <justaidajam@gmail.com>
2025-09-15 10:29:56 +0100

    #

--- src/main/java/io/kneo/broadcaster/controller/RadioController.java
+++ src/main/java/io/kneo/broadcaster/controller/RadioController.java
@@ -68,11 +68,15 @@ public class RadioController {
         router.route(HttpMethod.GET, "/radio/all-stations").handler(this::validateMixplaAccess).handler(this::getAllStations);
 
         router.route(HttpMethod.GET,  "/radio/:brand/submissions/files/:uploadId/stream").handler(this::streamProgress);
-        router.route(HttpMethod.POST, "/radio/:brand/submissions").handler(this::validateMixplaAccess).handler(this::submit);  //to save
-        router.route(HttpMethod.POST, "/radio/:brand/submissions/files/start").handler(jsonBodyHandler).handler(this::startUploadSession);  //to start sse progress feedback
+        router.route(HttpMethod.OPTIONS, "/radio/:brand/submissions/files/:uploadId/stream").handler(rc -> rc.response().setStatusCode(204).end());
+        router.route(HttpMethod.POST, "/radio/:brand/submissions").handler(this::validateMixplaAccess).handler(this::submit);
+        router.route(HttpMethod.GET, "/radio/:brand/submissions/files/start").handler(this::startUploadSession);
+        router.route(HttpMethod.OPTIONS, "/radio/:brand/submissions/files/start").handler(rc -> rc.response().setStatusCode(204).end());
+        router.route(HttpMethod.POST, "/radio/:brand/submissions/files/start").handler(jsonBodyHandler).handler(this::startUploadSessionDeprecated);
         router.route(HttpMethod.POST, "/radio/:brand/submissions/files/:id")
                 .order(-100)
                 .handler(this::uploadFile); // streaming upload
+        router.route(HttpMethod.OPTIONS, "/radio/:brand/submissions/files/:id").handler(rc -> rc.response().setStatusCode(204).end());
     }
 
     private void getPlaylist(RoutingContext rc) {
@@ -347,9 +351,16 @@ public class RadioController {
         UploadFileDTO uploadDto = fileUploadService.createUploadSession(uploadId, clientStartTimeStr);
         rc.response()
                 .putHeader("Content-Type", "application/json")
+                .putHeader("Cache-Control", "no-store, no-cache, must-revalidate")
+                .putHeader("Pragma", "no-cache")
                 .end(JsonObject.mapFrom(uploadDto).encode());
 
         LOGGER.info("Upload session started for uploadId: {}", uploadId);
+    }
+
+    @Deprecated
+    private void startUploadSessionDeprecated(RoutingContext rc) {
+        startUploadSession(rc);
     }
 
     private void uploadFile(RoutingContext rc) {

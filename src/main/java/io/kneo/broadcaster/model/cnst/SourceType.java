78aeb6f78c88bbd29103f2b1160f049bd7470ea0
kaira <justaidajam@gmail.com>
2025-09-21 21:43:45 +0100

    #

--- src/main/java/io/kneo/broadcaster/model/cnst/SourceType.java
+++ src/main/java/io/kneo/broadcaster/model/cnst/SourceType.java
@@ -1,5 +1,5 @@
     package io.kneo.broadcaster.model.cnst;
 
     public enum SourceType {
-        USERS_UPLOAD, RECOVERED_FROM_SPACES, @Deprecated ORPHAN_RECOVERY, SUNO_PROMPT, TEXT_FOR_TTS, SUBMISSION
+        USER_UPLOAD, RECOVERED_FROM_SPACES, SUNO_PROMPT, TEXT_FOR_TTS, SUBMISSION
     }

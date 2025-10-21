a4970b42c51641f592fd9f0d1bcadea3266b1fd6
kaira <justaidajam@gmail.com>
2025-10-21 15:25:08 +0100

    #

--- src/main/java/io/kneo/broadcaster/server/EnvConst.java
+++ src/main/java/io/kneo/broadcaster/server/EnvConst.java
@@ -4,4 +4,5 @@ public class EnvConst {
     public static final String APP_ID = "KneoBroadcaster" ;
     public static final String VERSION = "2.0.7" ;
     public static boolean DEV_MODE = true;
+    public static String VALIDATION_ERROR_PAGE = "https://mixpla.io/problems/validation-error";
 }

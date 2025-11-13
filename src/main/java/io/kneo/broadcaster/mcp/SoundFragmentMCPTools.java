2e30094b5fd38214652b434002773fc6658782fb
kaira <justaidajam@gmail.com>
2025-11-13 16:41:59 +0000

    #

--- src/main/java/io/kneo/broadcaster/mcp/SoundFragmentMCPTools.java
+++ src/main/java/io/kneo/broadcaster/mcp/SoundFragmentMCPTools.java
@@ -42,7 +42,7 @@ public class SoundFragmentMCPTools {
     }
 
     public int decideFragmentCount() {
-        return RANDOM.nextDouble() < 0.7 ? 2 : 1;
+        return RANDOM.nextDouble() < 0.3 ? 2 : 1;  //30% 2 songs
     }
 
     private Uni<List<SoundFragmentMcpDTO>> mapSoundFragmentsToAiDTO(List<SoundFragment> soundFragments) {

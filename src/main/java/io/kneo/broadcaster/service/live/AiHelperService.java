807e25eb9777be4e10b20893442f72abb7fc74de
kaira <justaidajam@gmail.com>
2025-11-18 18:25:24 +0000

    #

--- src/main/java/io/kneo/broadcaster/service/live/AiHelperService.java
+++ src/main/java/io/kneo/broadcaster/service/live/AiHelperService.java
@@ -76,9 +76,9 @@ public class AiHelperService {
     private final Randomizator randomizator;
 
     private static final List<RadioStationStatus> ACTIVE_STATUSES = List.of(
-            RadioStationStatus.ON_LINE,
-            RadioStationStatus.WARMING_UP,
-            RadioStationStatus.QUEUE_SATURATED  // be careful
+            RadioStationStatus.ON_LINE
+            //RadioStationStatus.WARMING_UP,
+            //RadioStationStatus.QUEUE_SATURATED  // be careful
     );
     
     private static final int SCENE_START_SHIFT_MINUTES = 5;
